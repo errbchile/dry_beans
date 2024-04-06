87492e638d363bc91319e662cbe8a0c606fdcd63
errbchile <errbchile@gmail.com>
2024-04-06 12:35:08 +0200

    rubocop romatting

--- spec/requests/api/collections_spec.rb
+++ spec/requests/api/collections_spec.rb
@@ -3,7 +3,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::Collections', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /api/itineraries/:itinerary_id/paths/:path_id/collections' do
+    it 'returns a list of collections for a specific path in JSON format' do
+      itinerary = Itinerary.create(driver: 'John Doe')
+      path = Path.create(itinerary:, departure: 'Location A', arrival: 'Location B')
+      collection1 = Collection.create(path:, product: 'product1', client: 'client1')
+      collection2 = Collection.create(path:, product: 'product2', client: 'client2')
+
+      get "/api/itineraries/#{itinerary.id}/paths/#{path.id}/collections"
+
+      expect(response).to have_http_status(200)
+
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq(2)
+      expect(json_response[0]['product']).to eq(collection1.product)
+      expect(json_response[1]['product']).to eq(collection2.product)
+      expect(json_response[0]['client']).to eq(collection1.client)
+      expect(json_response[1]['client']).to eq(collection2.client)
+    end
+  end
+
+  describe 'GET /api/itineraries/:itinerary_id/paths/:path_id/collections/:id' do
+    it 'returns a single collection for a specific path in JSON format' do
+      itinerary = Itinerary.create(driver: 'John Doe')
+      path = Path.create(itinerary:, departure: 'Location A', arrival: 'Location B')
+      collection = Collection.create(path:, product: 'product', client: 'client')
+
+      get "/api/itineraries/#{itinerary.id}/paths/#{path.id}/collections/#{collection.id}"
+
+      expect(response).to have_http_status(200)
+
+      json_response = JSON.parse(response.body)
+      expect(json_response['product']).to eq(collection.product)
+      expect(json_response['client']).to eq(collection.client)
+    end
   end
 end
--- spec/requests/api/paths_spec.rb
+++ spec/requests/api/paths_spec.rb
@@ -3,7 +3,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::Paths', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /api/itineraries/:itinerary_id/paths' do
+    it 'returns a list of paths for a specific itinerary in JSON format' do
+      itinerary = Itinerary.create(driver: 'John Doe')
+      path1 = Path.create(itinerary:, departure: 'Location A', arrival: 'Location B')
+      path2 = Path.create(itinerary:, departure: 'Location C', arrival: 'Location D')
+
+      get "/api/itineraries/#{itinerary.id}/paths"
+
+      expect(response).to have_http_status(200)
+
+      json_response = JSON.parse(response.body)
+      expect(json_response.length).to eq(2)
+      expect(json_response[0]['departure']).to eq(path1.departure)
+      expect(json_response[1]['departure']).to eq(path2.departure)
+    end
+  end
+
+  describe 'GET /api/itineraries/:itinerary_id/paths/:id' do
+    it 'returns a single path for a specific itinerary in JSON format' do
+      itinerary = Itinerary.create(driver: 'John Doe')
+      path = Path.create(itinerary:, departure: 'Location A', arrival: 'Location B')
+
+      get "/api/itineraries/#{itinerary.id}/paths/#{path.id}"
+
+      expect(response).to have_http_status(200)
+
+      json_response = JSON.parse(response.body)
+      expect(json_response['departure']).to eq(path.departure)
+      expect(json_response['arrival']).to eq(path.arrival)
+    end
   end
 end
