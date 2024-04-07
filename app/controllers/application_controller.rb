d95e8327d519eb1be4b76f52161f79d5994dc1d2
errbchile <errbchile@gmail.com>
2024-04-07 22:15:44 +0200

    response in json when bad request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  rescue_from ActionController::ParameterMissing do |e|
+    render json: { error: e.message }, status: :bad_request
+  end
 end
